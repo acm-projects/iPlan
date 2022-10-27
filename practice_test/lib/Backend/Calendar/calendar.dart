ce626ddb019dfa75dbec0cc2266832d4f2b5768d
Matthew Sheldon <punchwood2003@gmail.com>
2022-10-26 21:56:24 -0500

    Added the code that Sharun sent me

---
+++ practice_test/lib/Backend/Calendar/calendar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key});
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SfCalendar(
+        dataSource: MeetingDataSource(getAppointments()), //this is needed so that we are able to create the actual appointments
+      ),
+    );
+  }
+}
+
+class MeetingDataSource extends CalendarDataSource{
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  DateTime today = DateTime.now(); //DataTime is the variable type
+  DateTime startTime = DateTime(today.year, today.month, today.day); // the time the event starts at
+  DateTime endTime = startTime; //timing of how long the event actually takes
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: 'Dinner with friends', //name of the event
+      color: Colors.blue)); // color of what it will show up as on the calendar page
+
+  startTime = DateTime(today.year, today.month, today.day, 15, 30, 0); // timing for the second event
+  endTime = startTime.add(const Duration(hours: 1)); // length of second event
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: 'Take Dog for walk',
+      color: Colors.red));
+
+  startTime = DateTime(today.year, today.month, today.day, 7, 00, 0); // timing for the third event
+  endTime = startTime.add(const Duration(hours: 2, minutes: 30)); // length of third event
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: 'Breakfast with friends',
+      color: Colors.lightGreen));
+
+  return meetings;
+
+}
---
+++ practice_test/lib/Backend/Calendar/calendar_page.dart
@@ -0,0 +1,71 @@
+import 'task.dart';
+import 'calendar.dart';
+
+import '../Navigation_Bar/page_type.dart';
+
+/// @author [SharunNaicker]
+/// The [CalendarPage] class implements the use of classes like [Calendar]
+/// and [Task] to allow the user to create tasks which have a designated
+/// date they need to be completed by as well as a name for each task
+/// TODO: implement the link creation, compression, and expansion once other
+/// classes have been created.
+
+
+class CalendarPage {
+  /// The list of [Task] objects for the current event
+  late List<Task> _tasks;
+
+  /// The title of the event
+  late String _title;
+
+  /// The date of the event
+  late DateTime _date;
+
+  /// The check to see if a task has been finished or not
+  late bool _isComplete;
+
+  /// Creates a [CalendarPage] object with the passed [title] and [date]
+  /// parameters
+  CalendarPage({required String title, required DateTime date, required DateTime time,required bool isComplete }) {
+    _title = title;
+    _date = date;
+    // _time = time;
+    _tasks = <Task>[];
+    _isComplete = isComplete;
+  }
+
+  /// Returns a [PageType] object representing the page that the user should
+  /// be returned to given that they are currently on the Collaboration Page
+  PageType backButton() {
+    return PageType.homeScreen;
+  }
+
+  /// Returns the [_title] of the event
+  String getTitle() {
+    return _title;
+  }
+
+  // DateTime getTime() {
+  //   return _time;
+  // }
+
+  /// Returns the [_date] of the event
+  DateTime getDate() {
+    return _date;
+  }
+  bool getIsComplete() {
+    return _isComplete;
+  }
+
+  void addTask(
+      {required String title,
+       required DateTime date,
+        required DateTime time,
+       required bool isComplete}) {
+    _tasks.add(Task(
+      name: title,
+      date: date,
+      startTime: date,
+      isComplete: isComplete));
+  }
+}
